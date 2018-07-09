66efafe7d30a3eff46f7d6a6e18f68bbcc02f586
Thiyagu <thiyagu.1405@gmail.com>
2018-07-09 11:14:47 -0700

    third commit

--- drive.py
+++ drive.py
@@ -44,7 +44,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 10
+set_speed = 9
 controller.set_desired(set_speed)
 
 
@@ -61,7 +61,6 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB) ##changing the color space
         image_array=cv2.resize(image_array, (200,66), interpolation = cv2.INTER_AREA) ##resizing the image
         image_array=(image_array/127.5)-1.0 ##normalizing the image
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
